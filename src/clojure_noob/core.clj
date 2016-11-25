c18247813ec0ce6e8b4d78af299dc0c098358035
Rubin Simons <rubin@raaftech.com>
2016-11-25 15:30:03 +0100

    Added a few operator examples.

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -4,4 +4,30 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+
+  (if true
+    "The hammer!"
+    "This line won't be printed because it's else'd")
+
+  (if false
+    "This line won't be printed because it's then'd"
+    "The elbow!")
+
+  (if true
+    (do (println "Matched true!") ;; Then
+      "By Zeus's hammer!")
+    (do (println "Not true!") ;; Else
+      "By Aquaman's trident!"))
+
+  (when true
+    (println "A When operation!")
+    "abra cadabra")
+
+  (def failed-protagonist-names
+    ["Larry Potter" "Doreen the Explorer" "The Incredible Bulk"])
+
+
+  (def some-string
+    "String string")
+
   (println "I'm a little teapot!"))
