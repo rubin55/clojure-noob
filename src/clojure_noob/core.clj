f735aa669fffb4ad7e849c88fb365c8f66a8e205
Rubin Simons <rubin@raaf.tech>
2018-12-09 17:17:53 +0100

    Picked up Clojure for the Brave and True again..

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,41 +1,45 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(def asym-hobbit-body-parts
+  [{:name "head" :size 3}
+   {:name "left-eye" :size 1}
+   {:name "left-ear" :size 1}
+   {:name "mouth" :size 1}
+   {:name "nose" :size 1}
+   {:name "neck" :size 2}
+   {:name "left-shoulder" :size 3}
+   {:name "left-upper-arm" :size 3}
+   {:name "chest" :size 10}
+   {:name "back" :size 10}
+   {:name "left-forearm" :size 3}
+   {:name "abdomen" :size 6}
+   {:name "left-kidney" :size 1}
+   {:name "left-hand" :size 2}
+   {:name "left-thigh" :size 4}
+   {:name "left-knee" :size 2}
+   {:name "left-lower-leg" :size 3}
+   {:name "left-achilles" :size 1}
+   {:name "left-foot" :size 2}])
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+ "Expects a seq of maps that have a :name and  :size"
+ [asym-body-parts]
+ (loop [remaining-asym-parts asym-body-parts final-body-parts []]
+   (if (empty? remaining-asym-parts)
+     final-body-parts
+     (let [[part & remaining] remaining-asym-parts]
+       (recur remaining
+         (into final-body-parts
+           (set [part (matching-part part)])))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-
-  (if true
-    "The hammer!"
-    "This line won't be printed because it's else'd")
-
-  (if false
-    "This line won't be printed because it's then'd"
-    "The elbow!")
-
-  (if true
-    (do (println "Matched true!") ;; Then
-      "By Zeus's hammer!")
-    (do (println "Not true!") ;; Else
-      "By Aquaman's trident!"))
-
-  (when true
-    (println "A When operation!")
-    "abra cadabra")
-
-  (def failed-protagnist-names
-    ["Larry Potter" "Doreen the Explorer" "The Incredible Bulk"])
-
-  (def some-string
-    "String string")
-
-
-  (get [3 2 1] 0)
-
-  (println (conj [1 2 3] 4))
-  (println (conj '(1 2 3) 4))
-
-  (println (:a {:a 1 :b 2 :c 3}))
-  (println ({:a 1 :b 2 :c 3} :b))
-  (println (get {:a 1 :b 2 :c 3} :c))
+  (symmetrize-body-parts asym-hobbit-body-parts)
   (println "I'm a little teapot!"))
